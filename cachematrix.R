b1fa888eeeb203251419a10da8304fb7716e6080
Crazy88 <paul.lucas@target.com>
2015-04-24 14:23:26 -0500

    completed program 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix take a matrix as an input, stores the matrix with 
+## functions in a list
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){ 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve takes the makeCacheMatrix list as an input,
+## calculates the inverse if there is nothing in cache
+## references cache if the inverse is present
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        #if cached m is NOT null, return cache...
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        #else m is null, calc the inverse...
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+# test against Al's results
+
+# m <- matrix(c(-1, -2, 1, 1), 2,2)
+# x <- makeCacheMatrix(m)
+# x$get()
+# [,1] [,2]
+# [1,]   -1    1
+# [2,]   -2    1
+
+# first run:
+
+# inv<-cacheSolve(x)
+# >inv
+# [,1] [,2]
+# [1,]    1   -1
+# [2,]    2   -1
+
+# no cached data message, calculates and saves to cache, matches Al's
+
+# run again, see if cache is returned
+
+# >inv<-cacheSolve(x)
+# getting cached data
+# > inv
+# [,1] [,2]
+# [1,]    1   -1
+# [2,]    2   -1
+
+# cached data message, returns cache, matches Al's
+
+
+
+
+
+
+
+
+
